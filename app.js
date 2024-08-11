d9438efe4067e18279ba2391f1c5640486c90cb7
Sai Smruti Ranjan Das <saismrutiranjan1810@gmail.com>
2024-08-12 00:17:24 +0530

    Updated Portfolio

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
+/* To Download My Resume*/ 
 document.getElementById('downloadCvBtn').addEventListener('click', function() {
     const link = document.createElement('a');
     link.href = 'Resume/Sai Smruti Ranjan Das.pdf';  // Path to my CV file
     link.download = 'Resume/Sai Smruti Ranjan Das.pdf';  // Name the downloaded file
     link.click();
 });
-/*
 
-document.getElementById('homeBtn').addEventListener('click', function() {
-    window.location.href = 'index.html';  // URL of my home page
-});
-*/
-
-function redirectToHome() {
-    window.location.hash = '#home';
-  }
-  
-
+/* Hero Page Animated Text*/
 document.addEventListener('DOMContentLoaded', () => {
     var typed = new Typed('#typed', {
         strings: ["I'm into Web Development", "Currently, I'm learning Java"],
@@ -25,23 +16,26 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// scripts.js
 
+/* Skill Section Animate*/
 document.addEventListener('DOMContentLoaded', () => {
     const skillCircles = document.querySelectorAll('.skill-circle');
-
     skillCircles.forEach(circle => {
         circle.addEventListener('click', () => {
-            // Remove the animation class if it's already present to reset the animation
             circle.classList.remove('animate');
-            
-            // Force reflow to restart the animation
             void circle.offsetWidth;
-
-            // Add the animation class to trigger the animation
             circle.classList.add('animate');
         });
     });
+});
+
+/* Hamburger Menu*/
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+
 });
 
 
